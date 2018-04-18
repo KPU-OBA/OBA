aba92d6e8451483eb02a71110db067321d2cc343
LeeJeongWook <leejw7911@naver.com>
2018-04-18 23:07:21 +0900

    Block Switching

--- LeeJeongWook/Graduation-Work/Arduino/Block/Block.ino
+++ LeeJeongWook/Graduation-Work/Arduino/Block/Block.ino
@@ -1,12 +1,40 @@
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+const int greenLed = 12;
+const int redLed = 13;
+
+const int button01 = 1;
+const int button02 = 2;
+
+int button01_State = 0;
+int button02_State = 0;
+int buttonCount = 0;
+
+void setup(){
+  pinMode(greenLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  
+  pinMode(greenLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+
+  digitalWrite(greenLed, LOW);
+  digitalWrite(redLed, LOW);
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+void loop(){
+  button01_State = digitalRead(button01);
+  button02_State = digitalRead(button02);
+
+  if(button01_State == LOW){
+    digitalWrite(greenLed, HIGH);
+  }
+  else{
+    digitalWrite(greenLed,LOW);
+  }
+
+  if(button02_State == LOW){
+    digitalWrite(redLed, HIGH);
+  }
+  else{
+    digitalWrite(redLed,LOW);
+  }
 }
+
