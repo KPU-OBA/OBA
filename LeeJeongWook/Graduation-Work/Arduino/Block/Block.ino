775b1ed0973364e5de92fc859c66293b1baedd93
LeeJeongWook <leejw7911@naver.com>
2018-05-03 02:50:58 +0900

    Update Block.ino
    버튼카운트 수정 필요

--- LeeJeongWook/Graduation-Work/Arduino/Block/Block.ino
+++ LeeJeongWook/Graduation-Work/Arduino/Block/Block.ino
@@ -1,4 +1,18 @@
-#define IRRX A0
+
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+// OLED display TWI address
+#define OLED_ADDR   0x3C
+
+Adafruit_SSD1306 display(-1);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+#define IRRX A0 //적외선 센서 수광부
 
 //LED
 const int blueLed = 11;
@@ -16,22 +30,48 @@ boolean currentButton_01=LOW; //현재 버튼의 눌림 상태를 Boolean형 변
 boolean lastButton_02=LOW; //이전 버튼의 눌림 상태를 Boolean형 변수로 선언
 boolean currentButton_02=LOW; //현재 버튼의 눌림 상태를 Boolean형 변수로 선언
 
-int buttonCount = 0;
+int sensorButtonCount = 0;
+int displayButtonCount = 0;
 int val = 0;
 
+
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(9600); //Serial Monitor
+
+  //적외선 발광부
   pinMode(greenLed, OUTPUT);
   pinMode(redLed, OUTPUT);
   pinMode(blueLed, OUTPUT);
-
-  pinMode(button_01, INPUT);
-  pinMode(button_02, INPUT);
   
   digitalWrite(greenLed, LOW);
   digitalWrite(redLed, LOW);
   digitalWrite(blueLed, LOW);
+
+  //버튼
+  pinMode(button_01, INPUT);
+  pinMode(button_02, INPUT);
+
+  //OLED LCD
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  display.display();
+
+  //oledDisplay(displayButtonCount);
+  // display a pixel in each corner of the screen
+  display.drawPixel(0, 0, WHITE);
+  display.drawPixel(127, 0, WHITE);
+  display.drawPixel(0, 63, WHITE);
+  display.drawPixel(127, 63, WHITE);
+
+  // display a line of text
+  //display.setTextSize(3);
+  //display.setTextColor(WHITE);
+  //display.setCursor(0,20);
+  //display.print("Forward");
+  // update display with all of the above graphics
+  //display.display();
 }
+
 
 void loop(){
   int r = analogRead(IRRX);
@@ -45,28 +85,32 @@ void loop(){
   currentButton_01 = button_01_Debounce(lastButton_01); //디바운싱 처리된 버튼값을 읽음
   if(lastButton_01 == LOW && currentButton_01 == HIGH) //버튼을 누름
   {
-    buttonCount++;
+    sensorButtonCount++;
+    displayButtonCount++;
     Serial.println("=====================");
     Serial.println("Button_01");
     Serial.print("buttonCount: ");
-    Serial.println(buttonCount);
+    Serial.println(displayButtonCount);
     Serial.println("=====================");
+    oledDisplay(displayButtonCount);
   }
   lastButton_01=currentButton_01; //이전 버튼값을 현재 버튼값으로 변경
 
   currentButton_02 = button_02_Debounce(lastButton_02); //디바운싱 처리된 버튼값을 읽음
   if(lastButton_02 == LOW && currentButton_02 == HIGH) //버튼을 누름
   {
-    buttonCount--;
+    sensorButtonCount--;
+    displayButtonCount--;
     Serial.println("=====================");
     Serial.println("Button_02");
     Serial.print("buttonCount: ");
-    Serial.println(buttonCount);
+    Serial.println(displayButtonCount);
     Serial.println("=====================");
+    oledDisplay(displayButtonCount);
   }
   lastButton_02=currentButton_02; //이전 버튼값을 현재 버튼값으로 변경
 
-  ledSwitching(buttonCount);
+  ledSwitching(sensorButtonCount);
 }
 
 
@@ -74,28 +118,29 @@ boolean button_01_Debounce(boolean last){
   boolean current=digitalRead(button_01); //현재 버튼 상태를 확인
   if(last!=current) //이전 버튼 상태와 현재 버튼 상태가 다름
   {
-    delay(20); //5ms 동안 기다림
+    delay(20); //20ms 동안 기다림
     current=digitalRead(button_01);//current에 현재 버튼 상태를 저장
   }
   return current; //current 변수값을 반환
 }
 
+
 boolean button_02_Debounce(boolean last){
   boolean current=digitalRead(button_02); //현재 버튼 상태를 확인
   if(last!=current) //이전 버튼 상태와 현재 버튼 상태가 다름
   {
-    delay(20); //5ms 동안 기다림
+    delay(20); //20ms 동안 기다림
     current=digitalRead(button_02);//current에 현재 버튼 상태를 저장
   }
   return current; //current 변수값을 반환
 }
 
-void ledSwitching(int btnCount){
-  //LED스위칭
 
+//LED스위칭
+void ledSwitching(int btnCount){
   switch (btnCount) {
     case 0:
-      buttonCount += 7;
+      sensorButtonCount = 7;
       break;
     case 1:
       digitalWrite(greenLed, HIGH);
@@ -133,12 +178,65 @@ void ledSwitching(int btnCount){
       digitalWrite(blueLed, HIGH);
       break;
     case 8:
-      buttonCount -= 7;
+      sensorButtonCount = 1;
       break;
     default:
       digitalWrite(greenLed, LOW);
       digitalWrite(redLed, LOW);
       digitalWrite(blueLed, LOW);
   }
-  //return buttonCount;
+}
+
+//OLED_LCD
+void oledDisplay(int btnCount){
+  //display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  //display.display();
+  
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,20);
+  switch (btnCount) {
+    case 0:
+      display.print("7 is Empty");
+      display.display();
+      displayButtonCount = 7;
+      break;
+    case 1:
+      display.print("Forward");
+      display.display();
+      break;
+    case 2:
+      display.print("Turn Left");
+      display.display();
+      break;
+    case 3:
+      display.print("Turn Right");
+      display.display();
+      break;
+    case 4:
+      display.print("Function");
+      display.display();
+      break;
+    case 5:
+      display.print("Loop");
+      display.display();
+      break;
+    case 6:
+      display.print("6 is Empty");
+      display.display();
+      break;
+    case 7:
+      display.print("7 is Empty");
+      display.display();
+      break;
+    case 8:
+      display.print("Forward");
+      display.display();
+      displayButtonCount = 1;
+      break;
+    default:
+      display.clearDisplay();
+      display.display();
+  }
 }
